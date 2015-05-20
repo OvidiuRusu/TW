35ca00a0d18c1cfbb60e6c92a9547f10325d35d1
Vlad Tacutanu <tacutanu.vlad@gmail.com>
2015-05-20 16:14:41 +0300

    fac, univ, materii inserate

---
+++ inserari sql/inserari.sql
@@ -0,0 +1,19 @@
+/*INSERT INTO profesor(IdProfesor, Nume, Prenume, Email, Titlu, IdFacultate, IdAccount) VALUES (1, 'Buraga', 'Sabin', 'sabin.buraga@info.uaic.ro', 'Conf dr.', 1, 1);
+INSERT INTO profesor(IdProfesor, Nume, Prenume, Email, Titlu, IdFacultate, IdAccount) VALUES (2, 'Amariei', 'Ciprian', 'ciprian.amariei@info.uaic.ro', 'Prof', 1, 2);
+INSERT INTO profesor(IdProfesor, Nume, Prenume, Email, Titlu, IdFacultate, IdAccount) VALUES (3, 'Varlan', 'Cosmin', 'cosmin.varlan@info.uaic.ro', 'Lect.', 1, 3);*/
+
+INSERT INTO universitate VALUES(1,'Univ. Al. I. Cuza');
+  
+INSERT INTO facultate VALUES(101,'Facultatea de Informatica', 1);
+  
+INSERT INTO materie VALUES(1,'Algoritmi Genetici',NULL);
+INSERT INTO materie VALUES(2,'Limbaje formale, automate si compilatoare',NULL);
+INSERT INTO materie VALUES(3,'Retele',NULL);
+INSERT INTO materie VALUES(4,'Baze de date',NULL);
+INSERT INTO materie VALUES(5,'Algoritmica Grafurilor',NULL);
+INSERT INTO materie VALUES(6,'Limba Engleza III',NULL);
+INSERT INTO materie VALUES(7,'SGBD',NULL);
+INSERT INTO materie VALUES(8,'Tehnologii Web',NULL);
+INSERT INTO materie VALUES(9,'Programare Logica',NULL);
+INSERT INTO materie VALUES(10,'Ingineria Programarii',NULL);
+INSERT INTO materie VALUES(11,'Programare Avansata',NULL);

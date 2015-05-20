b9c0a361ec89a7ee41e4728c2fdc8d272c27aba0
Vlad Tacutanu <tacutanu.vlad@gmail.com>
2015-05-20 16:35:58 +0300

    populare un tabel cu materii

--- testbd.php
+++ testbd.php
@@ -1,12 +1,18 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<?php
 // Connecting, selecting database
 $link = mysql_connect('localhost', 'root', '')
     or die('Could not connect: ' . mysql_error());
-echo 'Connected successfully';
 mysql_select_db('tw') or die('Could not select database');
 
 // Performing SQL query
-$query = 'SELECT * FROM profesor';
+$query = 'SELECT * FROM materie';
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 // Printing results in HTML
@@ -26,3 +32,5 @@ mysql_free_result($result);
 // Closing connection
 mysql_close($link);
 ?>
+</body>
+</html>

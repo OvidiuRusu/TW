a543c63d066a11a1c46ad86c9b103b197e51e6f6
Emi94 <mitocariu.emilian@gmail.com>
2015-05-20 17:34:54 +0300

    LoginEmi

---
+++ loginEmi.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Student Login</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+  <h1><img src="images/oamlogo3.png" alt="" />Online <span style="font-weight:bold; color:#73868C;">Assignment</span> Manager</h1>
+</div>
+<div id="container">
+  <div id="navcontainer">
+    <ul id="nav">
+      <li><a href="signup.html">Sign Up</a></li>
+      <li><a href="login.html">Login</a></li>
+      <li><a href="index.html">How it Works</a></li>
+    </ul>
+  </div>
+<div id="content">
+  <div class="login-card">
+    <form method="post">
+      <input type="text" name="user" placeholder="Username">
+      <input type="password" name="pass" placeholder="Password">
+      <input type="submit" name="login" class="login login-submit" value="Login">
+    </form>
+	
+	<?php
+		require_once ('clasabd.php');
+		$bd = new DataBase_MySQL ('localhost', 'TW', 'root', '');
+		$bd->connect(); 
+		$bd->query("select username,password from account;");
+		$success=0;
+		while ($bd->next_record()) {
+			if($bd->Record["username"]==$_REQUEST["user"] && $bd->Record["password"]==$_REQUEST["pass"]) $success=1;
+		}
+	?>
+	<br>
+	<strong><?php print $success; ?></strong>
+	
+    <div class="login-help">
+      <a href="#">Forgot Password?</a>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>

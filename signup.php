d9b3558c86fc6b27c096e6b389715525d6718f81
VladTacutanu <tacutanu.vlad@gmail.com>
2015-05-25 19:52:36 +0300

    Schimbat 2 titluri de pagina

--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@ session_start();
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>Student Login</title>
+<title>Student Sign-Up</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="style.css" rel="stylesheet" type="text/css" />
 </head>

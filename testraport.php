f6d4005e9d5ad21ddae3906c679bd07db4aac22b
Vlad Tacutanu <tacutanu.vlad@gmail.com>
2015-06-01 14:49:07 +0300

    schema updatata bd

--- testraport.php
+++ testraport.php
@@ -4,22 +4,32 @@ include('connection.php');
 require("reporting/fpdf.php");
 class PDF extends FPDF
 {
-	function CreateTable($header,$data)
+	function CreateTable($header,$materie)
 	 {
 	 	// Column widths
-    $w = array(40, 35);
+    $w = array(53, 35);
     // Header
     for($i=0;$i<count($header);$i++)
         $this->Cell($w[$i],7,$header[$i],1,0,'C');
     $this->Ln();
     // Data
 
-    foreach($data as $row)
+    $nrstud = mysql_query("select student.IdStudent as id, student.Nume as n, student.Prenume as p from student
+left join student_materie on student.IdStudent=student_materie.IdStudent
+left join materie on student_materie.IdMaterie = materie.IdMaterie
+where student_materie.Status='In Curs'
+and materie.Nume='$materie'");
+    while($line = mysql_fetch_array($nrstud, MYSQL_ASSOC)) {
+        $this->Cell($w[0],6,$line["n"]." ".$line["p"],'LR');
+        $this->Ln();
+    }
+
+    /*foreach($data as $row)
     {
         $this->Cell($w[0],6,$row[0],'LR');
         $this->Cell($w[1],6,$row[1],'LR');
-        $this->Ln();
-    }
+       
+    } */
     // Closing line
     $this->Cell(array_sum($w),0,'','T');
 	 }
@@ -33,7 +43,7 @@ $pdf->SetFont('Arial','B',16);
 $pdf->Cell(10,10,''.$mat, 0,0,'C');
 $pdf->Cell(3,30,'Media temelor pentru fiecare student', 0,0,'C');
 $pdf->SetXY(10,40);
-$pdf->CreateTable($header,$header);
+$pdf->CreateTable($header,$mat);
 
 $pdf->Output();
 
